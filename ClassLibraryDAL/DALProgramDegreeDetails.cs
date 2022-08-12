512382fc64df5062ca66724e19d9e96d006e95df
Muhammad Awab <muhammadawab82002@gmail.com>
2022-08-12 17:20:56 +0500

    awab

--- ClassLibraryDAL/DALProgramDegreeDetails.cs
+++ ClassLibraryDAL/DALProgramDegreeDetails.cs
@@ -23,21 +23,26 @@ namespace ClassLibraryDAL
             while (sdr.Read())
             {
                 EntProgramDegreeDetails ee = new EntProgramDegreeDetails();
+
+
                 ee.ProgramDegreeDetailsId = sdr["ProgramDegreeDetailsId"].ToString();
+                ee.Programs = sdr["Programs"].ToString();
                 ee.Duration = sdr["Duration"].ToString();
                 ee.TotalSemesters = sdr["TotalSemesters"].ToString();
+                ee.Matric = sdr["Matric"].ToString();
+                ee.FSC = sdr["FSC"].ToString();
+                ee.BS = sdr["BS"].ToString();
                 ee.TotalFee = sdr["TotalFee"].ToString();
                 ee.SemesterFee = sdr["SemesterFee"].ToString();
                 ee.ClosingMerit = sdr["ClosingMerit"].ToString();
                 ee.ApprovedById = sdr["ApprovedById"].ToString();
-                ee.CityId = sdr["CityId"].ToString();
                 ee.Morning = sdr["Morning"].ToString();
                 ee.Evening = sdr["Evening"].ToString();
                 ee.Weekened = sdr["Weekened"].ToString();
+                ee.CityId = sdr["CityId"].ToString();
                 ee.PassingDegreeGroups = sdr["PassingDegreeGroups"].ToString();
                 ee.ProgramDegreeId = sdr["ProgramDegreeId"].ToString();
                 ee.InstituteId = sdr["InstituteId"].ToString();
-                ee.Percentage = sdr["Percentage"].ToString();
 
                 ProgramDegreeDetailList.Add(ee);
             }
@@ -46,7 +51,44 @@ namespace ClassLibraryDAL
         }
 
 
+        public static List<EntProgramDegreeDetails> GetProgramDegreeDetailsByInstituteId(string InstituteId)
+        {
+            SqlConnection con = DBHelper.GetConnection();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SP_GetProgramDegreeDetailsByInstituteId", con);
+            cmd.Parameters.AddWithValue("@InstituteId", int.Parse(InstituteId));
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataReader sdr = cmd.ExecuteReader();
+            List<EntProgramDegreeDetails> ProgramDegreeDetailList = new List<EntProgramDegreeDetails>();
+            while (sdr.Read())
+            {
+                EntProgramDegreeDetails ee = new EntProgramDegreeDetails();
 
+
+                ee.ProgramDegreeDetailsId = sdr["ProgramDegreeDetailsId"].ToString();
+                ee.Programs = sdr["Programs"].ToString();
+                ee.Duration = sdr["Duration"].ToString();
+                ee.TotalSemesters = sdr["TotalSemesters"].ToString();
+                ee.Matric = sdr["Matric"].ToString();
+                ee.FSC = sdr["FSC"].ToString();
+                ee.BS = sdr["BS"].ToString();
+                ee.TotalFee = sdr["TotalFee"].ToString();
+                ee.SemesterFee = sdr["SemesterFee"].ToString();
+                ee.ClosingMerit = sdr["ClosingMerit"].ToString();
+                ee.ApprovedById = sdr["ApprovedById"].ToString();
+                ee.Morning = sdr["Morning"].ToString();
+                ee.Evening = sdr["Evening"].ToString();
+                ee.Weekened = sdr["Weekened"].ToString();
+                ee.CityId = sdr["CityId"].ToString();
+                ee.PassingDegreeGroups = sdr["PassingDegreeGroups"].ToString();
+                ee.ProgramDegreeId = sdr["DegreeName"].ToString();
+                ee.InstituteId = sdr["InstituteId"].ToString();
+
+                ProgramDegreeDetailList.Add(ee);
+            }
+            con.Close();
+            return ProgramDegreeDetailList;
+        }
         //[ProgramDegreeDetailsId], [Duration], [TotalSemesters], [TotalFee], [SemesterFee], [ApprovedById], [CityId], [Morning], [Evening], [Weekened],
         //[PassingDegreeGroups], [ProgramDegreeId], [InstituteId], [Percentage]
         public static void SaveProgramDegreeDetails(EntProgramDegreeDetails ee)
@@ -54,20 +96,20 @@ namespace ClassLibraryDAL
             SqlConnection con = DBHelper.GetConnection();
             con.Open();
             SqlCommand cmd = new SqlCommand("SP_SaveProgramDegreeDetails", con);
-            cmd.Parameters.AddWithValue("@Duration", ee.Duration);
-            cmd.Parameters.AddWithValue("@TotalSemesters", ee.TotalSemesters);
-            cmd.Parameters.AddWithValue("@TotalFee", ee.TotalFee);
-            cmd.Parameters.AddWithValue("@SemesterFee", ee.SemesterFee);
-            cmd.Parameters.AddWithValue("@ClosingMerit", ee.ClosingMerit);
-            cmd.Parameters.AddWithValue("@ApprovedById", ee.ApprovedById);
-            cmd.Parameters.AddWithValue("@CityId", ee.CityId);
-            cmd.Parameters.AddWithValue("@Morning", ee.Morning);
-            cmd.Parameters.AddWithValue("@Evening", ee.Evening);
-            cmd.Parameters.AddWithValue("@Weekened", ee.Weekened);
+            //cmd.Parameters.AddWithValue("@Duration", ee.Duration);
+            //cmd.Parameters.AddWithValue("@TotalSemesters", ee.TotalSemesters);
+            //cmd.Parameters.AddWithValue("@TotalFee", ee.TotalFee);
+            //cmd.Parameters.AddWithValue("@SemesterFee", ee.SemesterFee);
+            //cmd.Parameters.AddWithValue("@ClosingMerit", ee.ClosingMerit);
+            //cmd.Parameters.AddWithValue("@ApprovedById", ee.ApprovedById);
+            //cmd.Parameters.AddWithValue("@CityId", ee.CityId);
+            //cmd.Parameters.AddWithValue("@Morning", ee.Morning);
+            //cmd.Parameters.AddWithValue("@Evening", ee.Evening);
+            //cmd.Parameters.AddWithValue("@Weekened", ee.Weekened);
             cmd.Parameters.AddWithValue("@PassingDegreeGroups", ee.PassingDegreeGroups);
             cmd.Parameters.AddWithValue("@ProgramDegreeId", ee.ProgramDegreeId);
-            cmd.Parameters.AddWithValue("@InstituteId", ee.InstituteId);
-            cmd.Parameters.AddWithValue("@Percentage", ee.Percentage);
+            cmd.Parameters.AddWithValue("@InstituteId", int.Parse(ee.InstituteId));
+
 
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.ExecuteNonQuery();
@@ -104,7 +146,7 @@ namespace ClassLibraryDAL
             cmd.Parameters.AddWithValue("@PassingDegreeGroups", ee.PassingDegreeGroups);
             cmd.Parameters.AddWithValue("@ProgramDegreeId", ee.ProgramDegreeId);
             cmd.Parameters.AddWithValue("@InstituteId", ee.InstituteId);
-            cmd.Parameters.AddWithValue("@Percentage", ee.Percentage);
+
 
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.ExecuteNonQuery();
@@ -138,7 +180,7 @@ namespace ClassLibraryDAL
                 ee.PassingDegreeGroups = sdr["PassingDegreeGroups"].ToString();
                 ee.ProgramDegreeId = sdr["ProgramDegreeId"].ToString();
                 ee.InstituteId = sdr["InstituteId"].ToString();
-                ee.Percentage = sdr["Percentage"].ToString();
+
             }
             con.Close();
             return ee;
--- ClassLibraryEntities/EntProgramDegreeDetails.cs
+++ ClassLibraryEntities/EntProgramDegreeDetails.cs
@@ -10,19 +10,25 @@ namespace ClassLibraryEntities
     {
         public string? ProgramDegreeDetailsId { get; set; }
         public string? Duration { get; set; }
+        public string? Programs { get; set; }
+        public string? Matric { get; set; }
+        public string? FSC { get; set; }
+        public string? BS { get; set; }
+
         public string? TotalSemesters { get; set; }
         public string? TotalFee { get; set; }
         public string? SemesterFee { get; set; }
         public string? ClosingMerit { get; set; }
         public string? ApprovedById { get; set; }
-        public string? CityId { get; set; }
         public string? Morning { get; set; }
         public string? Evening { get; set; }
         public string? Weekened { get; set; }
+        public string? CityId { get; set; }
+
         public string? PassingDegreeGroups { get; set; }
         public string? ProgramDegreeId { get; set; }
         public string? InstituteId { get; set; }
-        public string? Percentage { get; set; }
+        
 
 
 
