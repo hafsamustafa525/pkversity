a842508d1c691c5c43c1ecbc70e135eefaea4661
Muhammad Awab <muhammadawab82002@gmail.com>
2022-08-20 16:36:19 +0500

    updated

---
+++ ClassLibraryEntities/EntStudentInfo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryEntities
+{
+    public class EntStudentInfo
+    {
+
+
+        public string? Gender { get; set; }
+
+
+        public string? FirstName { get; set; }
+
+
+        public string? LastName { get; set; }
+
+        public string? CNIC { get; set; }
+
+
+        public DateTime? DateOfBirth { get; set; }
+
+
+        public string? City { get; set; }
+
+        public string? Province { get; set; }
+
+
+        public string? Address { get; set; }
+
+
+        public string? FatherName { get; set; }
+
+
+        public string? FatherCnic { get; set; }
+
+
+        public string? StudentMobileNo { get; set; }
+
+
+        public string? FatherMobileNo { get; set; }
+
+
+        public string? FatherIncome { get; set; }
+
+
+        public string? Email { get; set; }
+
+
+        public string? Password { get; set; }
+
+
+        public string? ConfirmPassword { get; set; }
+        public string? Designation{ get; set; }
+
+        public string? FAlive { get; set; }
+
+
+    }
+}
