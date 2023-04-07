5215e23e61a4454ab045b150c2fe0dcdaa2b412f
rehan985 <rehan.akhtar.985@gmail.com>
2023-04-08 03:01:55 +0500

    add docker file & Script Start.sh

--- ClassLibraryDAL/DBHelper.cs
+++ ClassLibraryDAL/DBHelper.cs
@@ -12,8 +12,8 @@ namespace ClassLibraryDAL
         public static SqlConnection GetConnection()
         {
            //SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=db_pkversity;Integrated Security=True");
-           SqlConnection con = new SqlConnection(@"Data Source=65.108.97.18;Initial Catalog=db_pkversity; User ID=user_pkversity; password=y^7R7lv28");
-          // SqlConnection con = new SqlConnection(@"Data Source=pkversity.database.windows.net;Initial Catalog=db_pkversity;User ID=db_pkversity;Password=earthandworld.985;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+          // SqlConnection con = new SqlConnection(@"Data Source=65.108.97.18;Initial Catalog=db_pkversity; User ID=user_pkversity; password=y^7R7lv28");
+           SqlConnection con = new SqlConnection(@"Data Source=pkversity.database.windows.net;Initial Catalog=db_pkversity;User ID=db_pkversity;Password=earthandworld.985;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             return con;
 
         }
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dotnet /App/Admin.dll &
+dotnet /App/User.dll  &
+dotnet /App/University.dll
