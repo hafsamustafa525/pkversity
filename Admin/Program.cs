a4e4f5878fe95a970ef75754ea51419e360fb4c3
rehanakhtar-at-594717084327 <rehan.akhtar.985@gmail.com>
2023-08-06 18:55:19 +0500

    pkversity staging

--- Admin/Program.cs
+++ Admin/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
 builder.WebHost.UseUrls("http://0.0.0.0:5004 ");
 
 var app = builder.Build();
