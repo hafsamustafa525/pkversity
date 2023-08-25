24718196b38b6f8142a282b9a18fe683485b7574
Muhammad-Awab <muhammadawab82002@gmail.com>
2023-08-25 19:24:09 +0500

    Add all files

--- Admin/Program.cs
+++ Admin/Program.cs
@@ -1,22 +1,28 @@
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
 builder.Services.AddServerSideBlazor();
 
-//builder.WebHost.UseUrls("http://0.0.0.0:5004 ");
+//builder.WebHost.UseUrls("http://0.0.0.0:5001 ");
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Error");
 }
-
 
 app.UseStaticFiles();
 
@@ -25,4 +31,5 @@ app.UseRouting();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
+// Configure Kestrel to listen on HTTP only
 app.Run();
